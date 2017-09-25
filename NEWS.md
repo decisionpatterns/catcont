d1a3ce5f926b642ae79aa70dec98d9892c497587
Christopher Brown <cbrown@decisionpatterns.com>
2017-09-25 02:37:36 -0700

    v0.3.0: Fix cat_cont

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# 2017-09-25 catcont 0.3.0
+
+ - Fix cat_cont to handle both atomic and recursive arguments
+
 # 2017-09-23: catcont 0.2.0
 
  - First CRAN release
--- R/cat_cont.R
+++ R/cat_cont.R
@@ -1,17 +1,21 @@
 
-#' @return 
-#' 
-#' `cat_cont` returns a named character with values either "`cat`" or "`cont`". 
-#' Names are the column names of `x`.
-#' 
-#' @examples 
-#' 
+#' @return
+#'
+#' `cat_cont` returns a named character with values either "`cat`" or "`cont`".
+#' If `x` is a atomic vector, a single string is given. If `x` is recursive,
+#' a "`cat`"/"`cont`" value is given for each element. Names correspond to the
+#' names of the element.
+#'
+#' @examples
+#'
 #'   data(iris)
 #'   cat_cont(iris)
-#' 
+#'
 #' @md
 #' @rdname catcont
 #' @export
 
-cat_cont <- function(x) 
-  ifelse( sapply(x, is_cat), "cat", "cont" )
+cat_cont <- function(x)
+  if( is.recursive(x) )
+    ifelse( sapply(x, is_cat), "cat", "cont" ) else
+    ifelse( is_cat(x), "cat", "cont" )
